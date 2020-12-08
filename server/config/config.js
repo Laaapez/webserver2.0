66b1d371ed10b309ff26c92a6f42acaaad24bf01
Francisco <lopezram.96@gmail.com>
2020-12-08 18:32:41 -0300

    Variabe de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://lopeztest:UVmsGgiWDZeWBP5e@cluster0.aoxar.mongodb.net/cafe';
+    urlDB = process.env.mongo_URI;
 }
 process.env.URLDATABASE = urlDB;
